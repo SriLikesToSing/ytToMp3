6f685e12d00b929540da8b0e342a2d105441a3f2
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2023-06-28 11:04:16 -0700

    finished backend. just gotta make the UI pretty and add the functionality for mp4 downloads as well

--- ytToMp3/Controllers/HomeController.cs
+++ ytToMp3/Controllers/HomeController.cs
@@ -7,16 +7,27 @@ using ytToMp3.Models;
 using System.Net.Http;
 using System.IO;
 using System.Net;
+using Microsoft.AspNetCore.Hosting.Server;
+using System.Security.Cryptography.Xml;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+using System.Linq;
 
 namespace ytToMp3.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly PhysicalFileProvider _fileProvider;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            string rootPath = Directory.GetCurrentDirectory();
+            _fileProvider = new PhysicalFileProvider(rootPath);
+
         }
         [HttpGet]
         public IActionResult Index()
@@ -28,39 +39,44 @@ namespace ytToMp3.Controllers
         public async Task<IActionResult> Index(string ytLink)
         {
             //save sessionID and path for file on db
-            
             var ytdl = new YoutubeDL();
             ytdl.YoutubeDLPath = "C:\\Users\\madhu\\Desktop\\Megafile\\Software\\yt-dlp.exe";
             ytdl.FFmpegPath = "C:\\PATH_Programs\\ffmpeg.exe";
             ytdl.OutputFolder = "C:\\Users\\madhu\\Desktop\\Megafile\\programming\\projects\\ytToMp3\\wwwroot\\lib\\links";
-            //  download the file to from server to user
-            string fileName = "here's a 10 second joke because you're busy.mp3";
 
+            //  download the file to from server to user
             var res = await ytdl.RunAudioDownload(ytLink, AudioConversionFormat.Mp3);
             string path = res.Data;
-            downloadFile(fileName, ytdl.OutputFolder);
+            string fileName = path.Split('\\').Last();
+            checkDelete(ytdl.OutputFolder);
 
-            return Content ($"Hello {path}");
+            return downloadFile(path, fileName);
+//            return Content($"Hello {path}");
         }
-
-        private async void downloadFile(string fileName, string filePath)
+        public void checkDelete(string directoryPath)
         {
-            /*
-            Response.Headers["Content-Disposition"] = "attachment;filename=" + fileName;
-            Response.Headers["Transfer-Encoding"] = "identity";
-            Response.SendFileAsync(filePath+"\\"+fileName);
-            Response.CompleteAsync(); 
-            */
-
-            string fi = filePath + "\\" + fileName;
-
-            using (WebClient Client = new WebClient())
-            {
-                Client.DownloadFile(fi);
-            }
-
+                //clear storage every few days or so
+                (from f in new DirectoryInfo(directoryPath).GetFiles()
+                 where f.CreationTime < DateTime.Now.Subtract(TimeSpan.FromDays(3))
+                 select f
+                 ).ToList()
+                    .ForEach(f => f.Delete());
         }
 
+        [HttpGet]
+        public ActionResult downloadFile(string fileLocation, string fileName)
+        {
+            var bytes = new byte[0];
+
+            using (var fs = new FileStream(fileLocation, FileMode.Open, FileAccess.Read))
+            {
+                var br = new BinaryReader(fs);
+                long numBytes = new FileInfo(fileLocation).Length;
+                var buff = br.ReadBytes((int)numBytes);
+                return File(buff, "audio/mpeg", fileName);
+            }
+            return null;
+        }
         public IActionResult Privacy()
         {
             return View();
